64c920d2148f39bb3c2a9eb573a3f2743ba76c3c
Tim Dill <brodt7258@gmail.com>
2019-05-12 14:59:13 -0400

    setup basic state.  state handed down to children correctly.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
-}
+import TodoList from './components/TodoComponents/TodoList';
 
-export default App;
+export default () => {
+  const [state, setState] = useState([
+    {
+      task: 'Organize Garage',
+      id: 1528817077286,
+      completed: false
+    },
+    {
+      task: 'Bake Cookies',
+      id: 1528817084358,
+      completed: false
+    }
+  ]);
+
+  const addTask = task => {
+    setState([ ...state, task ]);
+  };
+
+  const toggleCompletion = id => {
+    setState(state.map(e => e.id === id
+      ? { ...e, completed: !e.completed }
+      : e
+    ));
+  };
+
+  const deleteCompleted = () => {
+    setState(state.filter(e => !e.completed));
+  };
+
+  return (
+    <div>
+      <h2>Welcome to your Todo App!</h2>
+      <TodoList todos={state} />
+    </div>
+  );
+};
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,12 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+
+import Todo from './Todo';
+
+export default ({ todos }) => {
+  return (
+    <div>
+      <div>TodoList</div>
+      {todos.map((e, i) => <Todo taskData={e} key={i} />)}
+    </div>
+  );
+};
