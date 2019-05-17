dfec276a8aca7f50d2066de99194fc05cd831b71
Tim Dill <brodt7258@gmail.com>
2019-05-17 00:41:20 -0400

    wire/style header for theme

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Header.scss';
 
 import SearchBar from './SearchBar';
+import { ThemeContext } from '../../util/helpers'
 
 const Header = ({ searchVal, handleSearch }) => {
+  const darkMode = useContext(ThemeContext);
+
   return (
-    <header className="header-bar">
+    <header className={`header-bar ${darkMode ? 'dark-header' : 'light-header'}`}>
       <h1>
         React Todo
       </h1>
